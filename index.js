14cda410328aae03b04dbb36fd69cdb1d327a6a5
Xiaopei Li <xiaopei.li@geneegroup.com>
2014-05-27 17:34:58 +0800

    accept options

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 var net = require('net');
 
-var random_port = function(cb) {
-    /** @todo handle opts */
-    var from = 15000,
-        range = 100,
+var random_port = function() {
+    var cb,
+        opts = {};
+
+    if (arguments.length == 0) {
+        throw "no callback";
+    }
+    else if (arguments.length == 1) {
+        cb = arguments[0];
+    }
+    else {
+        opts = arguments[0];
+        cb = arguments[arguments.length - 1];
+    }
+
+    if (typeof cb != 'function') {
+        throw "callback is not a function";
+    }
+
+    if (typeof opts != 'object') {
+        throw "options is not a object";
+    }
+
+    var from = opts.from > 0 ? opts.from : 15000,
+        range = opts.range > 0 ? opts.range : 100,
         port = from + ~~(Math.random() * range);
+
+    /** @todo only root can listen to ports less than 1024 */
 
     var server = net.createServer();
     server.listen(port, function (err) {
@@ -14,7 +37,7 @@ var random_port = function(cb) {
         server.close();
     });
     server.on('error', function (err) {
-        get_port(cb);
+        random_port(opts, cb);
     });
 };
 
